16a14df3c59149d3323f94908636a472f956ae8d
Junichiro Ueno <jun116.u@gmail.com>
2012-03-24 20:00:10 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shepherd::Application.config.secret_token = 'efda3645749e50a29368472a28a357f161b572693abf63a72e6a0672b1463d56d4c8f341230b5bf4e6dfaf2d5fddf802f76ba1a37b732ef7f5bff6edbb5448c8'
